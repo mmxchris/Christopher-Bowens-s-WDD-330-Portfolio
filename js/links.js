aba2658c708cbd4bff6b7bd76b0ecd020529358a
Chris Bowen <50932159+mmxchris@users.noreply.github.com>
2022-01-08 21:20:46 -0500

    Added index and JS

---
+++ js/links.js
@@ -0,0 +1,18 @@
+const links =[
+    {
+        label: "Week 1 Notes",
+        url: "week1/index.html"
+    }
+    ]
+    
+    let lLen = links.length;
+    let text = "<ol>";
+    
+    for (let i = 0; i < lLen; i++)
+    {
+        text += "<li><a href=" + links[i].url + ">" + links[i].label + "</a></li>";    
+    }
+    
+    text += "</ol>";
+    
+    document.getElementById("content").innerHTML = text;
